3ba713d42eb6dd4b12c66ba0a293f305d5466176
wiwi_kuo <w22151500@kimo.com>
2023-10-17 11:20:03 +0800

    hotfix:add list jupyter

---
+++ get_stock_list_jupyter/bq_schema/stock_list.sql
@@ -0,0 +1,11 @@
+create table `bishare-1606.wiwi_test.taiwan_stock_list` (
+PageNumber STRING,
+ISIN        STRING,
+Stock_no  STRING,
+StockName STRING,
+MarketType STRING,
+StockType STRING,
+Industry STRING,
+IssueDate STRING,
+CFICode STRING,
+Remarks STRING )
